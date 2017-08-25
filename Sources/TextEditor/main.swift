0b19fbe866dc6d514153f1b0c7b40e3a0e6bd1c2
SirMrShyGuy <SirMrShyGuy@users.noreply.github.com>
2017-08-24 23:24:13 -0400

    More work done

--- Sources/TextEditor/main.swift
+++ Sources/TextEditor/main.swift
@@ -1,16 +1,37 @@
-import Foundation
+import Foundation // Using Foundation contains all the C libraies needed (alternative could be Darwin.c)
 
+var orig_termios = termios()
 
-func enableRawMode() {
-    var raw:termios = termios()
-
-    tcgetattr(STDIN_FILENO, &raw)
-    
+func disableRawMode() {
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios)
 }
 
+func enableRawMode() {
+    tcgetattr(STDIN_FILENO, &orig_termios)
+    atexit { disableRawMode() }
+    
+    var raw = orig_termios
 
-//var c:Int = 1
-var c:Character = "a"
+    //ECHO: prints key pressed into terminal
+    //ICANON: Turns off canonical mode changing input from byte to byte to line to line
+    let lflag = raw.c_lflag & ~UInt(ECHO | ICANON)
+    raw.c_lflag = lflag
+    
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw)
+}
 
-while read(STDIN_FILENO, &c, 1) == 1 && c != "q" {
+// -------------------
+// Program begins here
+// -------------------
+enableRawMode()
+
+var c:Int32 = 0
+// loops until q is entered
+while read(STDIN_FILENO, &c, 1) == 1 && c != 113 {
+    // displays value of key pressed
+    if iscntrl(c) == 1 {
+        print(c)
+    } else {
+        print("\(c) {\(Character(UnicodeScalar(Int(c))!))}")
+    }
 }
