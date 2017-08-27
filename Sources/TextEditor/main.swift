1da6a6bdb651001abbb02afd0d71f9bee3ce6ebe
SirMrShyGuy <SirMrShyGuy@users.noreply.github.com>
2017-08-27 00:13:26 -0400

    Raw mode implemented and begun user output setup

--- Sources/TextEditor/main.swift
+++ Sources/TextEditor/main.swift
@@ -1,43 +1,77 @@
 import Foundation // Using Foundation contains all the C libraies needed (alternative could be Darwin.c)
 
+// -----------------------
+// Setup code for raw mode
+// -----------------------
 var orig_termios = termios()
 
+func die(str:UnsafePointer<Int8>!) {
+    perror(str)
+    exit(1)
+}
+
 func disableRawMode() {
-    tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios)
+    if tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1 {
+        die(str: "tcsetattr")
+    }
 }
 
 func enableRawMode() {
-    tcgetattr(STDIN_FILENO, &orig_termios)
+    if tcgetattr(STDIN_FILENO, &orig_termios) == -1 { die(str: "tcsetattr") }
     atexit { disableRawMode() }
     
+    // creating a copy that can be modified so the program can revert to the original on exit
     var raw = orig_termios
 
     //ECHO: prints key pressed into terminal
     //ICANON: Turns off canonical mode changing input from byte to byte to line to line
     //ISIG: Turns off Ctrl-C and Ctrl-Z from sending there signals
     //IEXTEN: Turns off Ctrl-V from sending it's signal
-    let lflag = raw.c_lflag & ~UInt(ECHO | ICANON | ISIG | IEXTEN)
-    raw.c_lflag = lflag
+    raw.c_lflag = raw.c_lflag & ~UInt(ECHO | ICANON | ISIG | IEXTEN)
     //IXON: Turns off Ctrl-S and Ctrl-Q from sending there signals
     //ICRNL: Fixes Ctrl-M to sent 13 instead of 10
     //BRKINT: Flag is mostlikly irelevent for modern terminal emu but is considered for raw mode
     //INPCK: Flag is mostlikly irelevent for modern terminal emu but is considered for raw mode
     //ISTRIP: Flag is mostlikly irelevent for modern terminal emu but is considered for raw mode
-    let iflag = raw.c_iflag & ~UInt(IXON | ICRNL | BRKINT | INPCK | ISTRIP)
-    raw.c_iflag = iflag
+    raw.c_iflag = raw.c_iflag & ~UInt(IXON | ICRNL | BRKINT | INPCK | ISTRIP)
     //OPOST: Turns off translating \n from enter to \r\n
-    let oflag = raw.c_oflag & ~UInt(OPOST)
-    raw.c_oflag = oflag
+    raw.c_oflag = raw.c_oflag & ~UInt(OPOST)
     //CS8: Flag is mostlikly irelevent for modern terminal emu but is considered for raw mode
-    let cflag = raw.c_cflag & ~UInt(CS8)
-    raw.c_cflag = cflag
+    raw.c_cflag = raw.c_cflag & ~UInt(CS8)
     
+    // Using numbers beacuase raw.c_cc is a tuple
     //VTIME = 17
     //VMIN = 16
     raw.c_cc.17 = 1
     raw.c_cc.16 = 0
     
-    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw)
+    if tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1 { die(str: "tcsetattr") }
+}
+
+// ------------------
+// Code for user view
+// ------------------
+
+func editorReadKey() -> Int32 {
+    var c:Int32 = 0
+    var nread:Int = 0
+    while nread != 1 {
+        nread = read(STDIN_FILENO, &c, 1)
+        if nread == -1 && errno != EAGAIN { die(str: "Read") }
+    }
+    return c
+}
+
+func editorProcessKeypress() {
+    let c = editorReadKey()
+    
+    switch c {
+    case 17:
+        exit(0)
+    default:
+        break
+    }
+    
 }
 
 // -------------------
@@ -45,17 +79,6 @@ func enableRawMode() {
 // -------------------
 enableRawMode()
 
-var c:Int32 = 0
-// loops until q is entered
 while true {
-    // displays value of key pressed
-    read(STDIN_FILENO, &c, 1)
-    if iscntrl(c) == 1 {
-//        print(c)
-        print(c, terminator: "\r\n")
-    } else {
-//        print("\(c) {\(Character(UnicodeScalar(Int(c))!))}")
-        print("\(c) {\(Character(UnicodeScalar(Int(c))!))}", terminator: "\r\n")
-    }
-    if c == 113 { break }
+    editorProcessKeypress()
 }
